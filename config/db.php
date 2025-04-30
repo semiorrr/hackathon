0f0560b529ff9281d26955e3f4933bf8f1a2e2bd
Rjrances <rjrances301@gmail.com>
2025-04-30 14:04:00 +0800

    db ni

---
+++ config/db.php
@@ -0,0 +1,15 @@
+<?php
+$host = 'localhost';
+$db   = 'eseal';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+$pdo = new PDO($dsn, $user, $pass, $options);
+?>
