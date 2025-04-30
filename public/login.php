fc02396934c7d9223faed81349b8dbb474e8413c
Rjrances <rjrances301@gmail.com>
2025-04-30 17:24:45 +0800

    css

--- public/login.php
+++ public/login.php
@@ -1,6 +1,29 @@
-<form method="POST" action="../actions/login_check.php">
-    <input type="text" name="username" placeholder="Username" required><br>
-    <input type="password" name="password" placeholder="Password" required><br>
-    <button type="submit">Login</button>
-</form>
-<p>Don't have an account? <a href="register.php">Register here</a></p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login – eSeal Alert</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<header>
+    <h1>eSeal Alert</h1>
+    <nav>
+        <a href="login.php">Login</a>
+        <a href="register.php">Register</a>
+    </nav>
+</header>
+
+<div class="container">
+    <h2>Login to Dashboard</h2>
+    <form method="POST" action="../actions/login_check.php">
+        <input type="text" name="username" placeholder="Username" required><br><br>
+        <input type="password" name="password" placeholder="Password" required><br><br>
+        <button type="submit">Login</button>
+    </form>
+    <p>Don't have an account? <a href="register.php">Register here</a></p>
+</div>
+
+</body>
+</html>
--- public/register.php
+++ public/register.php
@@ -1,6 +1,29 @@
-<form method="POST" action="../actions/register_action.php">
-    <input type="text" name="username" placeholder="New Username" required><br>
-    <input type="password" name="password" placeholder="New Password" required><br>
-    <button type="submit">Register</button>
-</form>
-<p>Already have an account? <a href="login.php">Login here</a></p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Register – eSeal Alert</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<header>
+    <h1>eSeal Alert</h1>
+    <nav>
+        <a href="login.php">Login</a>
+        <a href="register.php">Register</a>
+    </nav>
+</header>
+
+<div class="container">
+    <h2>Create an Account</h2>
+    <form method="POST" action="../actions/register_action.php">
+        <input type="text" name="username" placeholder="New Username" required><br><br>
+        <input type="password" name="password" placeholder="New Password" required><br><br>
+        <button type="submit">Register</button>
+    </form>
+    <p>Already have an account? <a href="login.php">Login here</a></p>
+</div>
+
+</body>
+</html>
