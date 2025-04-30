f59b40dc11b28a45f3815b842820bce496cfbf67
Rjrances <rjrances301@gmail.com>
2025-04-30 22:23:47 +0800

    daghan

--- public/login.php
+++ public/login.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title>Login – LogiLock</title>
-    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="Login.css">
 </head>
 <body>
 
@@ -15,11 +16,11 @@
     </nav>
 </header>
 
-<div class="container">
+<div class="login-container">
     <h2>Login to Dashboard</h2>
     <form method="POST" action="../actions/login_check.php">
-        <input type="text" name="username" placeholder="Username" required><br><br>
-        <input type="password" name="password" placeholder="Password" required><br><br>
+        <input type="text" name="username" placeholder="Username" required><br>
+        <input type="password" name="password" placeholder="Password" required><br>
         <button type="submit">Login</button>
     </form>
     <p>Don't have an account? <a href="register.php">Register here</a></p>
